6c58dc19cd39bec0860df849c10861a71889ed7a
Er Amit Sinha (Tech Sinha Ji) <55745949+eramitsinha@users.noreply.github.com>
2023-06-15 10:53:51 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import Demo from './demo';
 import reportWebVitals from './reportWebVitals';
+import User from './User';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Demo />
+    <User />
   </React.StrictMode>
 );
 
