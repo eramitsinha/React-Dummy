a9a0d65d3791a900708626c308d4de7f42690c1c
Er Amit Sinha (Tech Sinha Ji) <55745949+eramitsinha@users.noreply.github.com>
2023-06-22 18:57:20 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,13 +5,32 @@ import App from './App';
 import Demo from './demo';
 import reportWebVitals from './reportWebVitals';
 import User from './User';
+import ClickEvent from './ClickEvent';
+import ClickEventArrow from './ClickEventArrow';
+import WithoutState from './WithoutState';
+import StateFunctionalComp from './StateFunctionalComp';
+import StateFunctionalComp2 from './StateFunctionalComp2';
+import StateClassComp from './StateClassComp';
+import StateClassComp2 from './StateClassComp2';
+import GetInputData1 from './GetInputData1';
+import GetInputData2 from './GetInputData2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    
     <Demo />
-    <User />
+    <User />  
+     <ClickEvent />
+    <ClickEventArrow />
+    <WithoutState /> 
+     <StateFunctionalComp /> 
+     <StateFunctionalComp2 />
+    <StateClassComp />
+    <StateClassComp2 />
+    <GetInputData1 />
+    <GetInputData2 />
   </React.StrictMode>
 );
 
