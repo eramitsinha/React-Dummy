7ad293d538bb134edd2021123deea434292aa892
Er Amit Sinha (Tech Sinha Ji) <55745949+eramitsinha@users.noreply.github.com>
2023-07-04 19:30:07 +0530

    Add files via upload

---
+++ src/Fragment1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Fragment1() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>This is a dummy para.</p>
+        </>
+    )
+}
---
+++ src/Fragment2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Fragment2() {
+  return (
+    <React.Fragment>
+        <h1>Hello World</h1>
+        <p>This is a dummy para.</p>
+    </React.Fragment>
+  )
+}
