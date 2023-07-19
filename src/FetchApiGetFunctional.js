13c760da374f66c3fcd3a760646158fed8888904
Er Amit Sinha (Tech Sinha Ji) <55745949+eramitsinha@users.noreply.github.com>
2023-07-19 22:43:49 +0530

    Add files via upload

--- src/FetchApiGetFunctional.js
+++ src/FetchApiGetFunctional.js
@@ -3,12 +3,23 @@ import React, { useEffect, useState } from 'react'
 export default function FetchApiGetFunctional() {
     const [data,setData] = useState([])
     useEffect(()=>{
-        fetch("http://localhost:3001/users").then((result)=>{
+        fetch("http://localhost:3000/users").then((result)=>{
             result.json().then((resp)=>{
                 setData(resp)
             })
         })
     },[])
+
+    function deleteUser(id)
+    {
+        fetch(`http://localhost:3000/users/${id}`,{
+            method : 'DELETE'
+        }).then((result)=>{
+            result.json()
+        }).then((response)=>{
+            console.log(response)
+        })
+    }
     return (
         <div>
             <h4>Fetch Data using GET Method - Functional Compoonent</h4>
@@ -28,6 +39,11 @@ export default function FetchApiGetFunctional() {
                             <td>{item.gender}</td>
                             <td>{item.company}</td>
                             <td>{item.email}</td>
+                            <td>
+                                <button onClick={()=>deleteUser(item.id)}>
+                                    Delete
+                                </button>
+                            </td>
                         </tr>
                     )
                 }
