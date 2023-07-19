1275f3b7d148e606cc2a1ac6a7240edd1fe986ca
Er Amit Sinha (Tech Sinha Ji) <55745949+eramitsinha@users.noreply.github.com>
2023-07-19 17:18:49 +0530

    Add files via upload

--- src/FetchApiPostFunctional.js
+++ src/FetchApiPostFunctional.js
@@ -6,44 +6,41 @@ export default function FetchApiPostFunctional() {
     const [company,setCompany] = useState("")
     const [gender,setGender] = useState("")
 
-    function save(e)
+    function save()
     {
-        e.preventDefault()
-
+        const data = {
+            name,
+            gender,
+            company,
+            email
+        }
         fetch('http://localhost:3001/users',{
             method : 'POST',
-            body: JSON.stringify({
-                name : name,
-                gender : gender,
-                company : company,
-                email : email
-            }),
+            body: JSON.stringify(data),
             headers:{"content-type":"application/json; charset=UTF-8"}  
         }).then((result)=>result.json()).then((response)=>{
             console.log(response)
         })
-        
-        
     }
 
     return (
         <div>
             <h4>Save Data using Fetch API - Functional Component</h4>
-            <form  onSubmit={save}>
+           
                 Name :
-                <input type="text" onChange={(e)=>setName(e.target.value)} value={name} />
+                <input type="text" value={name} onChange={(e)=>setName(e.target.value)} name="name"  />
                 <br />
                 Email :
-                <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} />
+                <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} name="email"  />
                 <br />
                 Company :
-                <input type="text" onChange={(e)=>setCompany(e.target.value)} value={company} />
+                <input type="text" value={company} onChange={(e)=>setCompany(e.target.value)} name="company" />
                 <br />
                 Gender :
-                <input type="text" onChange={(e)=>setGender(e.target.value)} value={gender} />
+                <input type="text" value={gender} onChange={(e)=>setGender(e.target.value)} name="gender" />
                 <br />
-                <button>Save</button>
-            </form>
+                <button type="button" onClick={save}>Save</button>
+           
         </div>
     )
 }
